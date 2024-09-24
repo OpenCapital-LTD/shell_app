91fc9161a8bc2d8067a9f5a95789f90d79078f21
Mc-shelton <sheltonnito@gmail.com>
2024-09-24 09:16:20 +0300

    updates

--- src/layout/mainlayout/index.jsx
+++ src/layout/mainlayout/index.jsx
@@ -5,16 +5,18 @@ import { Outlet } from "react-router-dom"
 import Floater from '../../components/floater'
 import useQuery from '../../hooks/query'
 import { useEffect } from 'react'
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 const MainLayout = () => {
-    const {path,param} = useQuery()
+    const { path, param } = useQuery()
     const navigate = useNavigate()
-    useEffect(()=>{
-        navigate(path + param)
-    },[])
+    useEffect(() => {
+        if (path) {
+            navigate(path + param)
+        }
+    }, [])
     return (
         <div className="app_container">
-            <Floater/>
+            <Floater />
             <Outlet />
         </div>
     )
