608739b3fb44ad885dee518d2d7299161fc56682
Mc-shelton <sheltonnito@gmail.com>
2024-12-10 01:02:27 +0300

    added socket

--- src/components/page/index.jsx
+++ src/components/page/index.jsx
@@ -1,6 +1,6 @@
 import React ,{ useEffect } from 'react';
 import '../../assets/styles/page.scss'
-const ws = new WebSocket('ws://localhost:3143');
+const ws = new WebSocket('ws://34.46.22.97:3143');
 import logo from  '../../assets/images/oca_short_logo.png'
 
 import { useState } from "react"
