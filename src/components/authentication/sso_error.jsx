8b888d5702d3743eebb54594bd5621e949b6feb5
Mc-shelton <sheltonnito@gmail.com>
2025-04-03 17:24:24 +0300

    share

--- src/components/authentication/sso_error.jsx
+++ src/components/authentication/sso_error.jsx
@@ -1,40 +1,48 @@
-import React, { useEffect, useState } from 'react'
-import useQuery from '../../hooks/query'
-import { jwtDecode } from 'jwt-decode'
-import { useNavigate } from 'react-router-dom'
-import Cookies from 'js-cookie'
-import { useGiraf } from '../../giraff'
-import '../../assets/styles/auth.scss'
+import React, { useEffect, useState } from "react";
+import useQuery from "../../hooks/query";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import { useGiraf } from "../../giraff";
+import "../../assets/styles/auth.scss";
 const SsoErrorPage = () => {
-    const { error } = useQuery()
-    const { gHead, addGHead } = useGiraf()
-    const navigation = useNavigate()
-    const [err, setErr] = useState()
-    
-    useEffect(() => {
-      let fun = Cookies.get('fun') || localStorage.getItem('fun')
-      if(error){
-        addGHead('error',error)
-        navigation('/auth_error')
-      }else{
-        if(gHead.error){
-            setErr(gHead.error)
-        }else{
-            if(fun){
-              navigation(fun)
-            }else{
-              navigation('/')
-            }
+  const { error } = useQuery();
+  const { gHead, addGHead } = useGiraf();
+  const navigation = useNavigate();
+  const [err, setErr] = useState();
+
+  useEffect(() => {
+    let fun = Cookies.get("fun") || localStorage.getItem("fun");
+    if (error) {
+      addGHead("error", error);
+      let player = error.split(":")[0].trim()
+      localStorage.setItem('user_id', player)
+      Cookies.set('user_id', player)
+      if (fun) {
+        navigation(fun);
+      } else {
+        navigation("/auth_error");
+      }
+    } else {
+      if (gHead.error) {
+        setErr(gHead.error);
+      } else {
+        if (fun) {
+          navigation(fun);
+        } else {
+          navigation("/");
         }
       }
-    }, [])
-    return (
-        <div className='auth_error'>
-        <h4 style={{
-
-        }}>403 : <br/>Authentication Forbidden</h4>
-        <p>{gHead.error}</p>
-        </div>
-    )
-}
-export default SsoErrorPage
+    }
+  }, []);
+  return (
+    <div className="auth_error">
+      <h4 style={{}}>
+        403 : <br />
+        Authentication Forbidden
+      </h4>
+      <p>{gHead.error}</p>
+    </div>
+  );
+};
+export default SsoErrorPage;
